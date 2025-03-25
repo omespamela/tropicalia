210f49beaf2ff3395dc17a5dd0460a7f4d202ca9
omespamela <135610195+omespamela@users.noreply.github.com>
2025-03-25 11:12:21 -0300

    s

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-document.addEventListener('DOMContentLoaded', function
-    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
-    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    const diminuiFonteBotao = document.getElementBtld('diminuir-fonte');
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
@@ -12,30 +10,17 @@ document.addEventListener('DOMContentLoaded', function
     });
 
     diminuiFonteBotao.addEventListener('click', function(){
-        tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-
+        tamanhoAtualFonte -=0.1
+        document.body.style.fontSize = '${tamanhoAtualFonte}ren';
+    
     });
-    .menu-acessibilidade{
-        position: fixed;
-        top:2rem;
-        right:20px;
-        z-index: 1000;
-        .rotacao-botao{ 
-        transform: rotate(-90deg);
-        transform-origin: right center;
-      }
-    });
-
-  });
-    .opcoes-acessibilidade{
-         margin-top:10px;
-         display: flex;
-         flex-direction: column;
-    }); 
-    botaoDeAcessibilidade.addEventListener('click', function (){
-        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
-       })
 
 });
+
+const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
+botaoDeAcessibilidade.addEventListener('click', function (){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+   })
