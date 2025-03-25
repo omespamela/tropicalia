56c8bbc23ffa2a9574e9e755dbdb2e5d78269d08
omespamela <135610195+omespamela@users.noreply.github.com>
2025-03-25 10:51:25 -0300

    s

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
@@ -14,5 +16,21 @@ document.addEventListener('DOMContentLoaded', function(){
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
     });
+    .menu-acessibilidade{
+        position: fixed;
+        top:2rem;
+        right:20px;
+        z-index: 1000;
+    });
+    .rotacao-botao{ 
+        transform: rotate(-90deg);
+        transform-origin: right center;
+  });
+    .opcoes-acessibilidade{
+         margin-top:10px;
+         display: flex;
+         flex-direction: column;
+    }); 
+
 
 });
